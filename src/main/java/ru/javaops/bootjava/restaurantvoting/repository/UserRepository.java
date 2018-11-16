b9267e738b82dd304adb04072420d710519d4035
gkislin <admin@javaops.ru>
2018-11-16 13:03:12 +0300

    1_03_query

--- src/main/java/ru/javaops/bootjava/restaurantvoting/repository/UserRepository.java
+++ src/main/java/ru/javaops/bootjava/restaurantvoting/repository/UserRepository.java
@@ -1,7 +1,18 @@
 package ru.javaops.bootjava.restaurantvoting.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 import ru.javaops.bootjava.restaurantvoting.model.User;
 
+import java.util.Optional;
+
+@Transactional(readOnly = true)
 public interface UserRepository extends JpaRepository<User, Integer> {
+
+    @Query("SELECT u FROM User u WHERE LOWER(u.email) = LOWER(:email)")
+    Optional<User> findByEmailIgnoreCase(@Param("email") String email);
+
+    Optional<User> findByLastNameContainingIgnoreCase(@Param("lastname") String lastName);
 }
